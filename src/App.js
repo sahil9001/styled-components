3f615a90fa332fcc458a5d61ea7ca65913de8e45
Sahil Silare <sahilsilare@gmail.com>
2021-05-01 16:22:28 +0530

    rm: Removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import GlobalStyle from "./globalStyles";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { IconContext } from "react-icons/lib";
-import { NavLink } from "react-router-dom";
 import { Button } from "../../globalStyles";
 import {
   Nav,
